5f661131eb19441d9e03ade82ceda030464eb938
ICC-ICZ <ICC-ICZ@ICC>
2017-03-28 17:22:13 +0200

    SudokuException

---
+++ src/entornostema4/SudokuException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornostema4;
+
+/**
+ *
+ * @author ICC-ICZ
+ */
+public class SudokuException extends Exception{
+     public SudokuException()
+    {
+        super();
+    }
+    
+}
