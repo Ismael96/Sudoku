4342344f899e1bc5615892b14961431c75c13569
ICC-ICZ <ICC-ICZ@ICC>
2017-03-28 17:54:37 +0200

    Variables privadas de la clase Sudoku.

--- src/entornostema4/Sudoku.java
+++ src/entornostema4/Sudoku.java
@@ -5,10 +5,66 @@
  */
 package entornostema4;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author ICC-ICZ
  */
 public class Sudoku {
-    
+// Variables privadas de la clase
+
+    private int fila;
+    private int columna;
+    private String resultadoFinal;
+    private boolean resultado;
+    private ArrayList<ArrayList<Integer>> lista;
+    private Random aleatorio;
+
+    public Sudoku() {
+        
+        fila=9;
+        columna=9;
+        resultadoFinal="";
+        resultado=true;
+        lista = new ArrayList<>();
+        aleatorio = new Random();
+        
+    }
+
+    public void inicializar() {
+    }
+
+    @Override
+    public String toString() {
+        String resultadoFinal = "";
+        return resultadoFinal;
+    }
+
+    public void modificarElemento(int fila, int columna, int elemento) throws SudokuException {
+    }
+
+    public void vaciarElemento(int fila, int columna) {
+    }
+
+    private boolean comprobarFila(int fila, int elemento) {
+        boolean resultado = true;
+        return resultado;
+    }
+
+    private boolean comprobarColumna(int columna, int elemento) {
+        boolean resultado = true;
+        return resultado;
+    }
+
+    private boolean comprobarCuadrante(int fila, int columna, int elemento) {
+        boolean resultado = true;
+        return resultado;
+    }
+
+    private boolean puedoInsertar(int fila, int columna, int elemento) {
+        boolean resultado = false;
+        return resultado;
+    }
 }
